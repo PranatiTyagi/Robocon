b372b8452a0676a5c9d20d8f851b14d31f9eb763
PranatiTyagi <45649610+PranatiTyagi@users.noreply.github.com>
2018-12-06 17:30:35 +0530

    Add files via upload

---
+++ encoder.ino
@@ -0,0 +1,32 @@
+ #define outputA 6
+ #define outputB 7
+ int counter = 0; 
+ int aState;
+ int aLastState;  
+ void setup() { 
+   pinMode (outputA,INPUT);
+   pinMode (outputB,INPUT);
+   
+   Serial.begin (9600);
+   // Reads the initial state of the outputA
+   aLastState = digitalRead(outputA);   
+ } 
+ void loop() { 
+   aState = digitalRead(outputA);
+  
+   if (aState != aLastState){     
+     if (digitalRead(outputB) != aState) { 
+       counter ++;
+     } else {
+       counter --;
+     }
+     Serial.print("Position: ");
+     Serial.println(counter);
+   } 
+   aLastState = aState; // Updates the previous state of the outputA with the current state
+   if(counter==2000)
+   {
+    //for distance radius is needed of encoder plate. 
+   }
+   }
+ 
